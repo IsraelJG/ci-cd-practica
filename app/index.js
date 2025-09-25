7367383fa885ad7cad18eb73bffeb9a0efdf5126
IsraelJG <isra.rayos@gmail.com>
2025-09-24 19:16:39 -0600

    Rompiendo el codigo

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
-Const express = require('express');
-Const app = express();
-Const port = 3000;
+const express = require('express');
+const app = express();
+const port = 3000;
+
 app.get('/', (req, res) => {
-res.send('¡Hola desde CI/CD!');
+  res.send('¡Hola desde CI/CD!');
 });
-app.listen(port,()=>{
-console.log(‘App escuchandoen http://localhost:${port}’);
+
+app.listen(port, () => {
+  console.log(`App escuchando en http://localhost:${port}`);
 });
